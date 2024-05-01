86ebbcdf95359ccba589f6ae3c7e8313175a3844
Alexander Lalejini <amlalejini@gmail.com>
2024-04-30 21:56:16 -0400

    Implement basic two-point crossover

--- include/program-synthesis/RecombinerLinearFunctionsProgram.hpp
+++ include/program-synthesis/RecombinerLinearFunctionsProgram.hpp
@@ -47,36 +47,6 @@ protected:
   double whole_func_swap_rate = 0.0;
   double per_func_seq_xover_rate = 0.0;
 
-  std::pair<size_t> FindTwoPoints(
-    emp::Random& random,
-    const function_t& func,
-  ) {
-    const size_t func_size = func.GetSize();
-    size_t start = random.GetUInt(0, func_size);
-    size_t end = random.GetUInt(0, func_size);
-    if (start > end) {
-      std::swap(start, end);
-    } else if ((start == end) && (start == 0)) {
-      // start and end both equal, set end to some percentage into function
-      const double percent = random.GetDouble(0, 1.0);
-      end = (size_t)(percent * (double)func_size);
-      emp_assert(end < func_size);
-    } else if ((start == end) && (start == (func_size - 1))) {
-      // start and end both equal to end of function, move start up some percentage
-      const double percent = random.GetDouble(0, 1.0);
-      const size_t move_up = (size_t)(percent * (double)func_size);
-      emp_assert(move_up < func_size);
-      start -= move_up;
-      emp_assert(start < end);
-    } else if (start == end) {
-      // start and end both equal and are both in the middle of the function
-      start = 0;
-    }
-    emp_assert(start < end);
-    emp_assert(end < func_size);
-    return {start, end};
-  }
-
 public:
 
   void SetWholeFuncSwapRate(double rate) {
@@ -152,27 +122,114 @@ public:
     */
     emp_assert(program_1.GetSize() > 0);
     emp_assert(program_2.GetSize() > 0);
-
+    std::cout << "Apply two-point recombination" << std::endl;
     // Generate vectors of function ids to use for determining which functions crossover
     emp::vector<size_t> p1_func_ids(program_1.GetSize(), 0);
     std::iota(p1_func_ids.begin(), p1_func_ids.end(), 0);
     emp::vector<size_t> p2_func_ids(program_2.GetSize(), 0);
     std::iota(p2_func_ids.begin(), p2_func_ids.end(), 0);
     // Determine the maximum number of functions we could xover
-    const size_t max_func_xovers = std::min(program_1.GetSize(), program_2.GetSize());
+    const size_t max_func_crosses = std::min(program_1.GetSize(), program_2.GetSize());
     // Shuffle order of function ids to allow us to guarantee unique pairings.
     emp::Shuffle(random, p1_func_ids);
     emp::Shuffle(random, p2_func_ids);
-    for (size_t i = 0; i < max_func_swaps; ++i) {
+    for (size_t i = 0; i < max_func_crosses; ++i) {
       if (i > 0 && (whole_func_swap_rate <= 0.0 || !random.P(per_func_seq_xover_rate))) {
         // If not first swap, swap rate > 0, and coin flip says don't swap, continue
         continue;
       }
-      // Get functions ids to be swapped
+      // Get functions ids to be recombined
       const size_t p1_func_id = p1_func_ids[i];
       const size_t p2_func_id = p2_func_ids[i];
-      // TODO - recombine two-point
+      std::cout << "Recombine p1[" << p1_func_id << "] and " << "p2["<< p2_func_id << "]" << std::endl;
+      emp_assert(p1_func_id < program_1.GetSize());
+      emp_assert(p2_func_id < program_2.GetSize());
+      emp_assert(program_1[p1_func_id].GetSize() > 0);
+      emp_assert(program_2[p2_func_id].GetSize() > 0);
+      // Pick two points for p1
+      const auto p1_crossover_points = FindTwoPoints(random, program_1[p1_func_id]);
+      std::cout << "  P1 crossover points: (" << p1_crossover_points.first << "," << p1_crossover_points.second << ")" << std::endl;
+      // Pick two points for p2
+      const auto p2_crossover_points = FindTwoPoints(random, program_2[p2_func_id]);
+      std::cout << "  P2 crossover points: (" << p2_crossover_points.first << "," << p2_crossover_points.second << ")" << std::endl;
+      // Create new p1 function
+      function_t new_p1_func(program_1[p1_func_id].GetTags());
+      // - (1) Copy p1 0 to p1_crossover_points.first
+      emp_assert(p1_crossover_points.first < program_1[p1_func_id].GetSize());
+      for (size_t inst_i = 0; inst_i < p1_crossover_points.first; ++inst_i) {
+        new_p1_func.PushInst(program_1[p1_func_id][inst_i]);
+      }
+      // - (2) Copy p2 starting at p2_crossover_points.first until (and including) p2_crossover_points.second
+      emp_assert(p2_crossover_points.first < program_2[p2_func_id].GetSize());
+      emp_assert(p2_crossover_points.second < program_2[p2_func_id].GetSize());
+      for (size_t inst_i = p2_crossover_points.first; inst_i <= p2_crossover_points.second; ++inst_i) {
+        new_p1_func.PushInst(program_2[p2_func_id][inst_i]);
+      }
+      // - (3) Copy p1 p1_crossover_points.second+1 to end of function
+      for (size_t inst_i = p1_crossover_points.second + 1; inst_i < program_1[p1_func_id].GetSize(); ++inst_i) {
+        new_p1_func.PushInst(program_1[p1_func_id][inst_i]);
+      }
+
+      // Create new p2 function
+      function_t new_p2_func(program_2[p2_func_id].GetTags());
+      // - (1) Copy from p2:
+      //        0 to p2_crossover_points.first
+      emp_assert(p2_crossover_points.first < program_2[p2_func_id].GetSize());
+      for (size_t inst_i = 0; inst_i < p2_crossover_points.first; ++inst_i) {
+        new_p2_func.PushInst(program_2[p2_func_id][inst_i]);
+      }
+      // - (2) Copy from p1:
+      //        starting at p1_crossover_points.first until (and including) p1_crossover_points.second
+      emp_assert(p1_crossover_points.first < program_1[p1_func_id].GetSize());
+      emp_assert(p1_crossover_points.second < program_1[p1_func_id].GetSize());
+      for (size_t inst_i = p1_crossover_points.first; inst_i <= p1_crossover_points.second; ++inst_i) {
+        new_p2_func.PushInst(program_1[p1_func_id][inst_i]);
+      }
+      // - (3) Copy from p2:
+      //        p2_crossover_points.second+1 to end of function
+      for (size_t inst_i = p2_crossover_points.second + 1; inst_i < program_2[p2_func_id].GetSize(); ++inst_i) {
+        new_p2_func.PushInst(program_2[p2_func_id][inst_i]);
+      }
+
+      program_1[p1_func_id] = new_p1_func;
+      program_2[p2_func_id] = new_p2_func;
     }
+  }
+
+  std::pair<size_t, size_t> FindTwoPoints(
+    emp::Random& random,
+    const function_t& func
+  ) {
+    const size_t func_size = func.GetSize();
+    emp_assert(func_size > 0);
+    if (func_size == 1) {
+      // If function only has 1 instruction, return start and finish as same
+      return {0, 0};
+    }
+
+    size_t start = random.GetUInt(0, func_size);
+    size_t end = random.GetUInt(0, func_size);
+    if (start > end) {
+      std::swap(start, end);
+    } else if ((start == end) && (start == 0)) {
+      // start and end both equal, set end to some percentage into function
+      const double percent = random.GetDouble(0, 1.0);
+      end = (size_t)(percent * (double)func_size);
+      emp_assert(end < func_size);
+    } else if ((start == end) && (start == (func_size - 1))) {
+      // start and end both equal to end of function, move start up some percentage
+      const double percent = random.GetDouble(0, 1.0);
+      const size_t move_up = (size_t)(percent * (double)func_size);
+      emp_assert(move_up < func_size);
+      start -= move_up;
+      emp_assert(start < end);
+    } else if (start == end) {
+      // start and end both equal and are both in the middle of the function
+      start = 0;
+    }
+    emp_assert(start < end);
+    emp_assert(end < func_size);
+    return {start, end};
   }
 
 
--- tests/Recombiner.cpp
+++ tests/Recombiner.cpp
@@ -46,37 +46,97 @@ TEST_CASE("RecombinerLinearFunctionsProgram") {
 
   emp::Random random(RANDOM_SEED);
 
-  recomb_t recombiner;
-  recombiner.SetWholeFuncSwapRate(0.8);
+  SECTION("WholeFunctionSwap") {
+    recomb_t recombiner;
+    recombiner.SetWholeFuncSwapRate(0.8);
 
-  program_t prog_1;
-  for (size_t f = 0; f < 3; ++f) {
-    prog_1.PushFunction(tag_t());
-    for (size_t i = 0; i < 8+f; ++i) {
-      prog_1.PushInst(inst_lib, "Nop-A", {-1*(int)f, -1*(int)f, -1*(int)f}, {tag_t()});
+    program_t prog_1;
+    for (size_t f = 0; f < 3; ++f) {
+      prog_1.PushFunction(tag_t());
+      for (size_t i = 0; i < 8+f; ++i) {
+        prog_1.PushInst(inst_lib, "Nop-A", {-1*(int)f, -1*(int)f, -1*(int)f}, {tag_t()});
+      }
+    }
+
+    program_t prog_2;
+    for (size_t f = 0; f < 8; ++f) {
+      prog_2.PushFunction(tag_t());
+      for (size_t i = 0; i < 3; ++i) {
+        prog_2.PushInst(inst_lib, "Nop-B", {(int)f, (int)f, (int)f}, {tag_t()});
+      }
+    }
+
+    std::cout << "--- Prog 1 before -- " << std::endl;
+    prog_1.Print(std::cout, inst_lib);
+    std::cout << std::endl;
+    std::cout << "--- Prog 2 before -- " << std::endl;
+    prog_2.Print(std::cout, inst_lib);
+    std::cout << std::endl;
+    recombiner.ApplyWholeFunctionRecombination(random, prog_1, prog_2);
+    std::cout << "--- Prog 1 after -- " << std::endl;
+    prog_1.Print(std::cout, inst_lib);
+    std::cout << std::endl;
+    std::cout << "--- Prog 2 after -- " << std::endl;
+    prog_2.Print(std::cout, inst_lib);
+    std::cout << std::endl;
+  }
+
+  SECTION("FindTwoPoints") {
+    recomb_t recombiner;
+    program_t prog;
+    for (size_t f = 0; f < 3; ++f) {
+      prog.PushFunction(tag_t());
+      for (size_t i = 0; i < 2*(f+1); ++i) {
+        prog.PushInst(inst_lib, "Nop-A", {-1*(int)f, -1*(int)f, -1*(int)f}, {tag_t()});
+      }
+    }
+    prog.PushFunction(tag_t());
+    prog.PushInst(inst_lib, "Nop-A", {0, 0, 0}, {tag_t()});
+
+    for (size_t f = 0; f < prog.GetSize(); ++f) {
+      std::cout << "prog[" << f << "], length = " << prog[f].GetSize() << std::endl;
+      for (size_t i = 0; i < 10; ++i) {
+        auto points = recombiner.FindTwoPoints(random, prog[f]);
+        std::cout << "(" << points.first << "," << points.second << ")" << std::endl;
+      }
     }
   }
 
-  program_t prog_2;
-  for (size_t f = 0; f < 8; ++f) {
-    prog_2.PushFunction(tag_t());
-    for (size_t i = 0; i < 3; ++i) {
-      prog_2.PushInst(inst_lib, "Nop-B", {(int)f, (int)f, (int)f}, {tag_t()});
+  SECTION("TwoPointCrossover") {
+    recomb_t recombiner;
+    recombiner.SetFuncSeqCrossoverRate(0.0);
+
+    program_t prog_1;
+    for (size_t f = 0; f < 3; ++f) {
+      prog_1.PushFunction(tag_t());
+      for (size_t i = 0; i < 20; ++i) {
+        prog_1.PushInst(inst_lib, "Nop-A", {-(int)f+1, (int)i, 0}, {tag_t()});
+      }
     }
+
+    program_t prog_2;
+    for (size_t f = 0; f < 8; ++f) {
+      prog_2.PushFunction(tag_t());
+      for (size_t i = 0; i < 20; ++i) {
+        prog_2.PushInst(inst_lib, "Nop-B", {(int)f+1, (int)i, 0}, {tag_t()});
+      }
+    }
+
+    std::cout << "--- Prog 1 before -- " << std::endl;
+    prog_1.Print(std::cout, inst_lib);
+    std::cout << std::endl;
+    std::cout << "--- Prog 2 before -- " << std::endl;
+    prog_2.Print(std::cout, inst_lib);
+    std::cout << std::endl;
+    recombiner.ApplyFunctionSequenceRecombinationTwoPoint(random, prog_1, prog_2);
+    std::cout << "--- Prog 1 after -- " << std::endl;
+    prog_1.Print(std::cout, inst_lib);
+    std::cout << std::endl;
+    std::cout << "--- Prog 2 after -- " << std::endl;
+    prog_2.Print(std::cout, inst_lib);
+    std::cout << std::endl;
   }
 
-  std::cout << "--- Prog 1 before -- " << std::endl;
-  prog_1.Print(std::cout, inst_lib);
-  std::cout << std::endl;
-  std::cout << "--- Prog 2 before -- " << std::endl;
-  prog_2.Print(std::cout, inst_lib);
-  std::cout << std::endl;
-  recombiner.ApplyWholeFunctionRecombination(random, prog_1, prog_2);
-  std::cout << "--- Prog 1 after -- " << std::endl;
-  prog_1.Print(std::cout, inst_lib);
-  std::cout << std::endl;
-  std::cout << "--- Prog 2 after -- " << std::endl;
-  prog_2.Print(std::cout, inst_lib);
-  std::cout << std::endl;
 
 }
+
